8679cda41cd0dcc57b96cc8d9530c02706feb66f
RolandHeer <roland.joachim.heer@hs-furtwangen.de>
2023-08-25 15:33:09 +0200

    ao

--- example/Script/Build/Script.js
+++ example/Script/Build/Script.js
@@ -121,10 +121,10 @@ var Script;
         }
         camNode.getChildren()[0].getComponent(ƒ.ComponentMist).activate(!toggleMist);
         camNode.getChildren()[0].getComponent(ƒ.ComponentBloom).activate(!toggleBloom);
-        //bbcamNode.getChildren()[0].getComponent(ƒ.ComponentAmbientOcclusion).activate(!toggleAo);
+        camNode.getChildren()[0].getComponent(ƒ.ComponentAmbientOcclusion).activate(!toggleAo);
         crc2.fillStyle = "#fff";
         crc2.font = canvas.height * 0.012 + "px sans-serif";
-        crc2.fillText("press T to toggle between new and old shading, press the Up or Down key to change the cameras pitch", canvas.height * 0.05, canvas.height * 0.07);
+        crc2.fillText("S to toggle old and new shading; M to toglle Mist, B to toggle Bloom and A to toggle AO; Press the Up or Down key to change the cameras pitch", canvas.height * 0.05, canvas.height * 0.07);
         let fps = Math.round(1000 / ƒ.Loop.timeFrameGame);
         crc2.fillText("FPS: " + fps, canvas.width * 0.95, canvas.height * 0.05);
         crc2.font = canvas.height * 0.02 + "px sans-serif";
